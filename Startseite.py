7f90a26bf2e9b21700ad122449e0c5ba27426558
Leonie Graf <gl2632@mci4me.at>
2026-02-22 00:50:56 +0100

    Solving small bug after merging

--- Startseite.py
+++ Startseite.py
@@ -9,10 +9,10 @@ st.title("SWD Abschlussprojekt", text_alignment="center")
 st.subheader("Joachim Spitaler und Leonie Graf", text_alignment="center")
 
 if st.button("Neue Modellierung starten", use_container_width=True):
+    init_session_states()
     init_default_session_states()
     st.switch_page("pages/1_Grundmaße.py")
 
 st.button("Letzte Berechnung wiederherstellen", use_container_width=True)
     #FOR THE FUTURE: Hier die Verknüpfung zu JSON, damit alte Daten aufgerufen werden
     #Dafür Session state aufrufen, der die JSON gespeicherten Daten beinhaltet
-
--- modules/state.py
+++ modules/state.py
@@ -12,22 +12,22 @@ Default = {
     "structure" : None,
 }
 
-def init_session_states():
+def init_session_states():                  #Eigentliches Initialisieren der Session States
     for key, value in Default.items():
         if key not in st.session_state:
             st.session_state[key] = value
 
-def init_default_session_states():
+def init_default_session_states():          #session_states auf default werte setzen
     for key, value in Default.items():
         st.session_state[key] = value
 
 def init_current_session_states():
     pass 
 
-def init_remove_input_force_support():
+def init_remove_input_force_support():      #Kraft und Lager entfernen
     st.session_state["forces"].clear()
     st.session_state["supports"].clear()
     st.session_state["ui_input_changed"] = False
 
-def init_empty_structure_state():
+def init_empty_structure_state():           #Structur leeren - wird nimma gnutzt
     st.session_state["structure"] = None
